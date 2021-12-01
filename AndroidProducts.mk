92d593ace80a9d1f0b7ff8060f83da66ffe0ebf0
jhonnytech90 <75139386+jhonnytech90@users.noreply.github.com>
2021-12-01 02:15:42 -0300

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a31.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_a31-user \
+    twrp_a31-userdebug \
+    twrp_a31-eng
